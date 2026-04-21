627dc22afbb4bc7dc18ac49c7a0b404cf7cc0559
Dheeraj101097 <dheerajap6@gmail.com>
2026-04-21 13:34:47 +0530

    env changed for dev

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
